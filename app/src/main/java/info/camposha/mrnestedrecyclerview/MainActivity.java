32e090d9f38de9a0efef6362d3cd65c61ebba119
Oclemy <oclemmi@gmail.com>
2019-02-25 04:40:09 +0300

    Initial commit

---
+++ app/src/main/java/info/camposha/mrnestedrecyclerview/MainActivity.java
@@ -0,0 +1,30 @@
+package info.camposha.mrnestedrecyclerview;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new NestedParentAdapter());
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                outRect.set(16, 16, 16, 0);
+            }
+        });
+    }
+
+}
